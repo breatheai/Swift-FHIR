41d464e6680374a18087c38e6269d394779ba2eb
Alan Cooper <ascooper57@users.noreply.github.com>
2025-02-27 10:48:05 -0800

    simplified test setup

--- Tests/ClientTests/XCTestCase+Local.swift
+++ Tests/ClientTests/XCTestCase+Local.swift
@@ -22,14 +22,8 @@ extension XCTestCase {
         return proj.appendingPathComponent("TestResources")
 	}
     
-    class var testsDirectory: String {
-        let dir = #file as NSString
-        let proj = ((dir.deletingLastPathComponent as NSString).deletingLastPathComponent as NSString).deletingLastPathComponent as NSString
-        return proj.appendingPathComponent("fhir-parser/downloads")
-    }
-    
     public func readJSONFile(_ filename: String, directory: String? = nil) throws -> FHIRJSON {
-        let dir = directory ?? type(of: self).testsDirectory
+        let dir = directory ?? testResourcesDirectory
         XCTAssertTrue(FileManager.default.fileExists(atPath: dir), "You must either first download the FHIR spec or manually adjust `XCTestCase.testsDirectory` to point to your FHIR download directory")
         
         let path = (dir as NSString).appendingPathComponent(filename)
